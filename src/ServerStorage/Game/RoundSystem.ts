4efa48c15da1cd4cb5938f9412a1c26598ddde24
AymenCode33 <aymenpro966@gmail.com>
2025-01-29 18:34:58 +0100

    Intiallize Game with module

---
+++ src/ServerStorage/Game/RoundSystem.ts
@@ -0,0 +1,10 @@
+// Services
+
+const Maps = game.GetService("ServerStorage").FindFirstChild("Maps");
+const Players = game.GetService("Players");
+
+// Fonctions
+
+function StartGame() {
+	const EligiblePlayer = {};
+}
